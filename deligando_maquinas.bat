ec90bbc29a10e61e7fdca46d783668888a97f500
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2023-05-17 08:32:09 -0300

    Update and rename reiniciar_maquinas.bat to deligando_maquinas.bat

--- deligando_maquinas.bat
+++ deligando_maquinas.bat
@@ -1,6 +1,6 @@
 @echo off
 for /f "tokens=*" %%a in (maquinas.txt) do (
-echo Reiniciando a maquina: %%a
+echo Desligando a maquina: %%a
 shutdown -s -t 0 -f -m \\%%a
 )
-echo Todas as maquinas foram reiniciadas com sucesso!
+echo Todas as maquinas foram desligadas com sucesso!
